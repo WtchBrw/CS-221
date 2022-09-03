6b2bfc9768e9856c18608cd98ecfae675910b313
Nick C <60305140+Depressee-Chan@users.noreply.github.com>
2022-09-02 18:33:20 -0600

    Add files via upload

---
+++ GridMonitor/GridMonitor.java
@@ -0,0 +1,161 @@
+package GridMonitor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class GridMonitor implements GridMonitorInterface {
+    private double[][] baseGrid;
+    private double[][] sumGrid;
+    private double[][] averageGrid;
+    private double[][] deltaGrid;
+    private boolean[][] dangerGrid;
+
+    /**
+     * @author Nick Codispoti
+     * 
+     * @param fname is the file that contains grid values
+     * @throws FileNotFoundException thrown if the file does not exist
+     */
+    @SuppressWarnings("resource")
+	public GridMonitor(String fname) throws FileNotFoundException {
+        if (fname == null || fname.isEmpty()) {
+        	throw new FileNotFoundException();
+        } else {
+        	int dimX = -1;
+        	int dimY = -1;
+	        File inputFile = new File(Paths.get("").toAbsolutePath().toString(), fname);
+	        Scanner inputScanner = new Scanner(inputFile);
+	        
+	        while (inputScanner.hasNext()) {
+	            String next = inputScanner.next();
+	            if (dimY == -1) {
+	                try {
+	                    dimY = Integer.parseInt(next);
+	                } catch (NumberFormatException err) {
+	                    err.printStackTrace();
+	                    return;
+	                }
+	            } else if (dimX == -1) {
+	                try {
+	                    dimX = Integer.parseInt(next);
+	                    break;
+	                } catch (NumberFormatException err) {
+	                    err.printStackTrace();
+	                    return;
+	                }
+	            }
+	        }
+	
+	        baseGrid = new double[dimY][dimX];
+	        sumGrid = new double[dimY][dimX];
+	        averageGrid = new double[dimY][dimX];
+	        deltaGrid = new double[dimY][dimX];
+	        dangerGrid = new boolean[dimY][dimX];
+	
+	        yloop: for (int y = 0; y < dimY; y++) {
+	            for (int x = 0; x < dimX; x++) {
+	                if (!inputScanner.hasNext()) break yloop;
+	                try {
+	                    baseGrid[y][x] = Double.parseDouble(inputScanner.next());
+	                } catch (NumberFormatException err) {
+	                    err.printStackTrace();
+	                    return;
+	                }
+	            }
+	        }
+	
+	        inputScanner.close();
+	        for (int y = 0; y < dimY; y++) {
+	            for (int x = 0; x < dimX; x++) {
+	                double above, below, left, right, sum;
+	                
+	                if (y == 0) above = baseGrid[y][x];
+	                else above = baseGrid[y-1][x];
+	                if (y == dimY-1) below = baseGrid[y][x];
+	                else below = baseGrid[y+1][x];
+	                
+	                if (x == 0) left = baseGrid[y][x];
+	                else left = baseGrid[y][x-1];
+	                if (x == dimX-1) right = baseGrid[y][x];
+	                else right = baseGrid[y][x+1];
+	
+	                sum = above + below + left + right;
+	                sumGrid[y][x] = sum;
+	                averageGrid[y][x] = sum/4;
+	                deltaGrid[y][x] = Math.abs(sum/8);
+	                dangerGrid[y][x] =
+	                        baseGrid[y][x] < averageGrid[y][x] - deltaGrid[y][x] ||
+	                                baseGrid[y][x] > averageGrid[y][x] + deltaGrid[y][x];
+	            }
+	        }
+        }
+    }
+
+    @Override
+    public double[][] getBaseGrid() {
+        //return a copy of baseGrid
+        return copy2DArray(baseGrid);
+    }
+
+    @Override
+    public double[][] getSurroundingSumGrid() {
+        //return a copy of sumGrid
+        return copy2DArray(sumGrid);
+    }
+
+    @Override
+    public double[][] getSurroundingAvgGrid() {
+        //return a copy of averageGrid
+        return copy2DArray(averageGrid);
+    }
+
+    @Override
+    public double[][] getDeltaGrid() {
+        //return a copy of deltaGrid
+        return copy2DArray(deltaGrid);
+    }
+
+    @Override
+    public boolean[][] getDangerGrid() {
+        //return a copy of dangerGrid
+        return copy2DArray(dangerGrid);
+    }
+
+    /**
+     * create a backup of the array
+     */
+    private double[][] copy2DArray(double[][] toCopy) {
+        double[][] toReturn = new double[toCopy.length][toCopy[0].length];
+
+        for (int y = 0; y < toCopy.length; y++) {
+            System.arraycopy(toCopy[y], 0, toReturn[y], 0, toCopy[y].length);
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * creates a copy of the array
+     */
+    private boolean[][] copy2DArray(boolean[][] toCopy) {
+        boolean[][] toReturn = new boolean[toCopy.length][toCopy[0].length];
+
+        for (int y = 0; y < toCopy.length; y++) {
+            System.arraycopy(toCopy[y], 0, toReturn[y], 0, toCopy[y].length);
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * returns a string representation of the grid
+     */
+    @SuppressWarnings("unused")
+	@Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        return getBaseGrid().toString();
+    }
+}
---
+++ GridMonitor/readme.txt
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------
+****************
+* DLL Project
+* BSU CS221
+* 9/2/2022
+* Nick Codispoti
+****************
+
+OVERVIEW:
+
+
+
+ - INCLUDED FILES:
+
+ * GridMonitor.java - source file
+ * GridMonitorInterface.java - source file
+ * GridMonitorTest.java - source file
+ * readme.txt - this file
+ 
+ - COMPILING AND RUNNING:
+
+ From the directory containing all source files, compile the
+ driver class (and all dependencies) with the command:
+ $ javac GridMonitor.java
+
+ Run the compiled class file with the command:
+ $ java GridMonitor
+
+ Console output will give the results of all tests after the program
+ finishes executing.
+ 
+ - PROGRAM DESIGN AND IMPORTANT CONCEPTS:
+
+ The GridMonitor.java file contains all the tests that can be run on the grid files
+ being used. The test files are to be supplied by the user.
+ 
+ The GridMonitorInterface.java file contains all the methods that needed to be defined
+ within GridMonitor. However, the implementation of the class could
+ easily be removed after the methods are all defined in order to cut down on the
+ program size and number of dependencies. The test files were removed from this
+ package for that reason.
+ 
+ - TESTING:
+ Getting the program to work was tricky at first because it has been a long time
+ since I last used Java to program. However after attending a few lectures surrounding the
+ concepts used in this program, the process of developing it became exponentially easier.
+ 
+----------------------------------------------------------------------------
